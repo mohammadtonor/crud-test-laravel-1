f4f0ba590de654f83fc0ee9ed08c15fe3ab54a78
mt1398 <Mtonor1368@gmail.com>
2023-05-11 23:05:38 +0430

    add migration customer domain

--- Domains/Customer/database/migrations/2023_05_11_253234create_customers_table.php
+++ Domains/Customer/database/migrations/2023_05_11_253234create_customers_table.php
@@ -1,11 +1,11 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,15 +13,16 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name',50)->nullable();
-            $table->string('last_name',50)->nullable();
-            $table->date('date_of_birth')->nullable();
-            $table->string('phone_number', 20)->unique();
-            $table->string('email',100)->unique();
-            $table->string('bank_account_number')->nullable();
+            $table->string('Firstname',50);
+            $table->string('Lastname',50);
+            $table->date('DateOfBirth');
+            $table->bigInteger('PhoneNumber')->unsigned()->unique();
+            $table->string('Email')->unique();
+            $table->string('BankAccountNumber')->nullable();
+            $table->softDeletes();
             $table->timestamps();
 
-            $table->unique(['first_name', 'last_name', 'date_of_birth']);
+            $table->unique(['Firstname', 'Lastname', 'DateOfBirth']);
         });
     }
 
